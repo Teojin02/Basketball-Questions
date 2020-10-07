7edc7e3ea060ff04587906467c9e80fbb9d026a3
Your Name <you@example.com>
2020-10-06 22:29:33 -0700

    added images, updated index.css, update app.js

--- basketball/src/App.js
+++ basketball/src/App.js
@@ -30,6 +30,66 @@ export default function App () {
                 {answerText: 'Magic Johnson ', isCorrect: false},
             ],
         },
+          {
+            questionsText: "When did Micheal Jordan won his first MPV ?",
+            answerOptions: [
+                {answerText: '1985-86', isCorrect: false},
+                {answerText: '1992-1993', isCorrect: false},
+                {answerText: '1987-88', isCorrect: true},
+                {answerText: '1998-99', isCorrect: false},
+            ],
+        },
+          {
+            questionsText: "Who invented basketball?",
+            answerOptions: [
+                {answerText: 'Micheal Jordan', isCorrect: false},
+                {answerText: 'Adam Silver', isCorrect: false},
+                {answerText: 'Tyrone Corbin', isCorrect: false},
+                {answerText: 'James Naismith', isCorrect: true},
+            ],
+        },
+          {
+            questionsText: "Who won the NBA championship in 2016?",
+            answerOptions: [
+                {answerText: 'Cleveland Cavaliers', isCorrect: true},
+                {answerText: 'Golden State Warriors', isCorrect: false},
+            ],
+        },
+           {
+            questionsText: "Who's the Greatest Player Of All Time?",
+            answerOptions: [
+                {answerText: 'Lebron James', isCorrect: true},
+                {answerText: 'Kobe Bryant', isCorrect: true},
+                {answerText: 'Micheal Jordan', isCorrect: true},
+                {answerText: "shaquille O'neal", isCorrect: true},
+            ],
+        },
+           {
+            questionsText: "Who was the first player to be drafted in the NBA",
+            answerOptions: [
+                {answerText: 'Dan Gadzuric', isCorrect: false},
+                {answerText: 'Clifton McNeeley', isCorrect: true},
+                {answerText: 'Tyrone Corbin', isCorrect: false},
+                {answerText: 'Earl Lloyd', isCorrect: false},
+            ],
+        },
+           {
+            questionsText: "Who wom the dunk const back in 2016?",
+            answerOptions: [
+                {answerText: 'Zach Lavine', isCorrect: true},
+                {answerText: 'Aaron Gordon', isCorrect: false},
+            ],
+        },
+           {
+            questionsText: "Who won rookie of the year 2020?",
+            answerOptions: [
+                {answerText: 'Zion Williamson', isCorrect: false},
+                {answerText: 'Brandon Clarke', isCorrect: false},
+                {answerText: 'Ja Morant', isCorrect: true},
+                {answerText: 'Kendrick Nunn', isCorrect: false},
+            ],
+        },
+
     ];
 
 const [currentQuestions, setCurrentQuestions]= useState (0);
@@ -53,7 +113,7 @@ return (
     <div className="app">
         {showScore ? (
             <div className="score-section">
-                You Scored {score} out of 3
+                You Scored {score} out of 10
             </div>
         ) : (
             <>
